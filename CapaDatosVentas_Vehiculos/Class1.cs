d1eaea14879b84eb738e260895467cf9a7ee735f
wilber <wilber201@hotmail.com>
2023-06-29 09:29:40 -0400

    Agregados: Usuario(Insert,Update,Consultation), Solicitud_Detalle(Insert,Update,Consultation)

---
+++ CapaDatosVentas_Vehiculos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatosVentas_Vehiculos
+{
+    public class Class1
+    {
+    }
+}
---
+++ CapaNegocioVenta_Vehiculo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocioVenta_Vehiculo
+{
+    public class Class1
+    {
+    }
+}
